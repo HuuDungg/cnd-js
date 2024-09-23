79e0c6c2cf1c64889673a9fd873da8d8cbe8e563
huudungg <huudung2004kt@gmail.com>
2024-09-23 09:15:32 +0700

    add menu

--- testjs.js
+++ testjs.js
@@ -1,2 +1,20 @@
 alert("this is a simple js file")
-console.log("hi all")
+// Ẩn nội dung menu cũ
+var originalMenu = document.querySelector('.header-wrapper');
+originalMenu.style.display = 'none';
+
+// Tạo menu mới
+var newMenu = `
+  <nav class="new-menu">
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
+`;
+
+// Chèn menu mới vào trong .header-wrapper
+originalMenu.insertAdjacentHTML('afterend', newMenu);
+
